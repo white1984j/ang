aed68b71b0c51c787b41d0e235475ca07181e804
white1984j <white1984j@gmail.com>
2018-07-26 01:28:14 +0300

    add 404 page

---
+++ app/404.php
@@ -0,0 +1,42 @@
+<? include('header.php'); ?>
+
+		<section class="section-404">
+			<div class="container">
+				<div class="section-404__title">
+					<h1>Ошибка 404</h1>
+				</div>
+				<div class="section-404__subtitle">
+					<p>Страница не найдена</p>
+				</div>
+				<div class="section-404__txt">
+					<p>Проверьте, правильно ли вы ввели адрес. Правильно? Тогда, возможно, на сайте идут технические работы. Обновите страницу через пару минут или прямо сейчас закажите звонок и мы поможем решить вашу проблему:</p>
+				</div>
+				<div class="b-404">
+					<div class="b-404-form">
+						<form action="#">
+							<label class="b-label ">
+								<p class="b-label__txt">Имя</p>
+                <input type="text" class="b-input">
+              </label>
+              <label class="b-label ">
+								<p class="b-label__txt">Телефон</p>
+                <input type="tel" class="b-input">
+              </label>
+							<label class="b-label b-label--checkbox">
+                <input type="checkbox" >
+                <div class="b-checkbox-icon">
+                  <span class="icon icon-checked"></span>
+                </div>
+                <div class="b-checkbox-text">Согласие на обработку данных</div>
+              </label>
+              <input type="submit" class="b-404-form__btn" value="ЗАКАЗАТь звонок">
+						</form>
+					</div>
+					<div class="b-404-img">
+						<img src="img/404.png" alt="404">
+					</div>
+				</div>
+			</div>
+		</section>
+
+<? include('footer.php'); ?>
