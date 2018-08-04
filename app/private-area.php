07322ca8d4786df2031bd92a49c8fea953eece09
white1984j <white1984j@gmail.com>
2018-08-04 19:36:04 +0300

    add personal-area page

---
+++ app/private-area.php
@@ -0,0 +1,60 @@
+<? include('header.php'); ?>
+
+<section class="section-main">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="b-breadcrumbs">
+          <ul class="b-breadcrumbs__ul">
+            <li class="b-breadcrumbs__li">
+              <a href="#" class="b-breadcrumbs__link">Главная</a>
+            </li>
+            <li class="b-breadcrumbs__li">
+              <a href="#" class="b-breadcrumbs__link">Личный кабинет</a>
+            </li>
+          </ul>
+        </div>
+        <h2>Личный кабинет</h2>
+        <br>
+        <div class="b-private-areas-wrap">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <a href="#" class="b-pravate-areas-category">
+                <div class="b-pravate-areas-category__img">
+                  <div class="icon icon-private-info"></div>
+                </div>
+                <span class="b-pravate-areas-category__name">Личные данные</span>
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <a href="#" class="b-pravate-areas-category">
+                <div class="b-pravate-areas-category__img">
+                  <div class="icon icon-currents-orders"></div>
+                </div>
+                <span class="b-pravate-areas-category__name">Текущие заказы</span>
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <a href="#" class="b-pravate-areas-category">
+                <div class="b-pravate-areas-category__img">
+                  <div class="icon icon-history-orders"></div>
+                </div>
+                <span class="b-pravate-areas-category__name">История заказов</span>
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+              <a href="#" class="b-pravate-areas-category">
+                <div class="b-pravate-areas-category__img">
+                  <div class="icon icon-cart"></div>
+                </div>
+                <span class="b-pravate-areas-category__name">Корзина</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<? include('footer.php'); ?>
